99af5a06353802540c33b5c31797c4377b50e528
Zhao Li <zhaol@hawaii.edu>
2020-12-01 11:39:55 -1000

    enable view to query and list greetings

--- greetings/views.py
+++ greetings/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Greeting
 
 # Create your views here.
 from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the greetings index.")
+class IndexView(generic.ListView):
+    template_name = 'greetings/index.html'
+    context_object_name = 'greeting_list'
+
+    def get_queryset(self):
+        """Return the all greetings."""
+        return Greeting.objects.all()
